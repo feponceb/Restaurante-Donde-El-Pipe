09e17b606cff6354c199126735a95942a1e9537c
unknown <SSDD@PNAWSA05LC54528.duoc.acad>
2026-05-20 18:30:50 -0400

    Creacion base del gestorInventario

---
+++ gestorInventario/src/main/java/com/dondeElPipe/gestorInventario/GestorInventarioApplication.java
@@ -0,0 +1,13 @@
+package com.dondeElPipe.gestorInventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorInventarioApplication.class, args);
+	}
+
+}
---
+++ gestorInventario/src/test/java/com/dondeElPipe/gestorInventario/GestorInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dondeElPipe.gestorInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
